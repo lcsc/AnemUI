9b645ab778575c06d70e6133db842b2e8a02e5f6
Eduardo Moreno <eduardo.moreno@ipe.csic.es>
2024-12-11 13:38:40 +0100

    Fix indentation

--- anemui-core/src/data/CsDataTypes.ts
+++ anemui-core/src/data/CsDataTypes.ts
@@ -51,7 +51,7 @@ export interface CsTimesJsData{
     times: CsTimesJsVarData<string> // Dates with data
     varMin: CsTimesJsVarData<number> //Min value for date
     varMax: CsTimesJsVarData<number> //Max value for date
-minVal: CsTimesJsVarData<number> //Min value for variable
+    minVal: CsTimesJsVarData<number> //Min value for variable
     maxVal: CsTimesJsVarData<number> //Max value for variable
 
     //Data of chunks
