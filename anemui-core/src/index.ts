64a72c65468ac95fc4f510424be53a1890f0d8ba
Eduardo Moreno <eduardo.moreno@ipe.csic.es>
2025-08-18 13:39:14 +0200

    feat(core): export downloadXYChunk in public API
    Expose downloadXYChunk from './data/ChunkDownloader' via '@lcsc/anemui-core' to allow visors to import through the public surface.

--- anemui-core/src/index.ts
+++ anemui-core/src/index.ts
@@ -5,5 +5,6 @@ export * from './ServiceApp';
 export { BaseApp } from './BaseApp';
 export { InfoDiv } from './ui/InfoPanel';
 export { downloadUrl } from './data/UrlDownloader';
+export { downloadXYChunk } from './data/ChunkDownloader';
 export { renderers, getFolders } from './tiles/Support';
 export * from './data/CsDataTypes';
